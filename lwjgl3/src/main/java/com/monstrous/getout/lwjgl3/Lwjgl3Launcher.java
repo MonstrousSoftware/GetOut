f88825aba182149d944a730e331d84a0b86b1719
Monstrous <49096535+MonstrousSoftware@users.noreply.github.com>
2024-09-16 17:23:52 +0200

    updated launchers

--- lwjgl3/src/main/java/com/monstrous/getout/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/monstrous/getout/lwjgl3/Lwjgl3Launcher.java
@@ -17,7 +17,7 @@ public class Lwjgl3Launcher {
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("GetOut");
+        configuration.setTitle("Get Out!");
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
         configuration.useVsync(true);
@@ -27,9 +27,15 @@ public class Lwjgl3Launcher {
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setWindowedMode(640, 480);
+        configuration.setWindowedMode(1200, 800);
+        // anti-aliasing
+        configuration.setBackBufferConfig(8, 8, 8, 8, 16, 0, 4);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        configuration.setWindowIcon("monstrous128.png", "monstrous64.png", "monstrous32.png", "monstrous16.png");
+
+        // Use OpenGL 3.2 to emulate GL ES 3.1
+        // needed for AA frame buffer
+//        configuration.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.GL31, 3,2);
         return configuration;
     }
 }
